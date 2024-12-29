6d6f613e93ff5ba2b6c0c031dddad236ed2ef0cf
naosuke884 <884naoki.general@gmail.com>
2024-12-29 01:33:49 +0000

    chore: init server

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module github.com/naosuke884/hey-you
+
+go 1.22.2
+
+require github.com/gorilla/mux v1.8.1
---
+++ server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/naosuke884/hey-you/handler"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", handler.HomeHandler)
+	http.Handle("/", router)
+
+	srver := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srver.ListenAndServe())
+}
